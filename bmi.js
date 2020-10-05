41006520d138411bae30b9cd6213c4a5c7a225c1
Cory <coryodonoghue1@gmail.com>
2020-10-05 18:15:35 +0100

    lab2 with comments

--- bmi.js
+++ bmi.js
@@ -1,15 +1,16 @@
 class BMI{
     constructor(height, weight){
+        //initilsing the arguments.
         this.height = height;
         this.weight = weight;
     }
-    calculateBMI(){
+    calculateBMI(){//carries out formula to calculate bmi.
         let bmi = this.weight/(this.height ** 2);
-        return bmi;
+        return bmi;//returns bmi value.
     }
 
 }
 
-let MyBMI = new BMI(2, 90);
-let calculatedBMI = MyBMI.calculateBMI();
-console.log(calculatedBMI);
+let MyBMI = new BMI(2, 90);//creating an instance of the class.
+let calculatedBMI = MyBMI.calculateBMI();//Assigning returned value to a variable.
+console.log(calculatedBMI);//printing value to screen.
--- cars.js
+++ cars.js
@@ -1,32 +1,33 @@
-class Vehicle {
+class Vehicle {//Parent Class.
     constructor(make, model, year) {
+        //initialising Variables.
         this.make = make;
         this.model = model;
         this.year = year;
     }
 
 
-    information() {
+    information() {//method to print make,miodel and year to the screen.
         console.log(` Make:${this.make}, Model: ${this.model},year:  ${this.year}`);
     }
 }
 
-class car extends Vehicle {
+class car extends Vehicle {//child class .
     constructor(make,model,year,door) {
+        //invoking instructer class using super keyword.
       super(make,model,year);
-      this.door = door; 
+      this.door = door; //initialising door.
     }
 
-    information(){
+    information(){//method calls information() from parent class.
         super.information();
-        console.log(`Doors: ${this.door}`);
+        console.log(`Doors: ${this.door}`);// Outputs number of doors to console.
     }
 }
 
-let myVehicle = new Vehicle('audi', 'r8', 2018);
-let newVehicle = myVehicle.information();
-console.log(newVehicle);
+let myVehicle = new Vehicle('audi', 'r8', 2018);//creating an instance of the parent class.
+let newVehicle = myVehicle.information();//assigning data from myVehicle to newVehicle to print to screen.
+console.log(newVehicle);//prints data to console.
 
-let myCar = new car('VW', 'r10',2019,4);
-let newCar = myCar.information();
-console.log(newCar);
+let myCar = new car('VW', 'r10',2019,4);//creating instance of chiild class
+let newCar = myCar.information();//Assigning data to newCar & printing to console from info() function.
